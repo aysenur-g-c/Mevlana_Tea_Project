5c3b311bbf498db4955af2a6add043d8007d9a75
ahyat <gokdemir.aysenur@gmail.com>
2025-09-22 02:21:54 +0300

    Elements, TC_04 güncellendi.

--- src/test/java/Pages/TC_04_FOOT.java
+++ src/test/java/Pages/TC_04_FOOT.java
@@ -1,6 +1,8 @@
 package Pages;
 
 import Utility.BaseDriverParameter;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -15,6 +17,7 @@ public class TC_04_FOOT extends BaseDriverParameter {
         Elements elements=new Elements(driver);
         driver.get("https://mevlanacay.de/tr/");
 
+        waitForVisible(elements.imgClose).click();
         hover(elements.footerAddress);
         waitForVisible(elements.footerAddress);
         waitForVisible(elements.footerMail);
@@ -27,8 +30,10 @@ public class TC_04_FOOT extends BaseDriverParameter {
         driver.get("https://mevlanacay.de/tr/");
         String originalWindow = driver.getWindowHandle();
 
+        waitForVisible(elements.imgClose).click();
         hover(elements.footerFacebookIcon);
-        waitForClickable(elements.footerFacebookIcon);
+
+        new Actions(driver).moveToElement(elements.footerFacebookIcon).click().perform();
         new WebDriverWait(driver, Duration.ofSeconds(10))
                 .until(driver -> driver.getWindowHandles().size() > 1);
         for (String windowHandle : driver.getWindowHandles()) {
@@ -44,6 +49,66 @@ public class TC_04_FOOT extends BaseDriverParameter {
                 "Redirection to Facebook page failed.");
         driver.close();
         driver.switchTo().window(originalWindow);
+
+        new Actions(driver).moveToElement(elements.footerYoutubeIcon).click().perform();
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(driver -> driver.getWindowHandles().size() > 1);
+        for (String windowHandle : driver.getWindowHandles()) {
+            if (!windowHandle.equals(originalWindow)) {
+                driver.switchTo().window(windowHandle);
+                break;
+            }
+        }
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.urlToBe("https://www.youtube.com/channel/UCAz9J9R6tvp3K76s9zor3bA"));
+        Assert.assertEquals(driver.getCurrentUrl(),
+                "https://www.youtube.com/channel/UCAz9J9R6tvp3K76s9zor3bA",
+                "Redirection to Facebook page failed.");
+        driver.close();
+        driver.switchTo().window(originalWindow);
+
+        new Actions(driver).moveToElement(elements.footerXIcon).click().perform();
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(driver -> driver.getWindowHandles().size() > 1);
+        for (String windowHandle : driver.getWindowHandles()) {
+            if (!windowHandle.equals(originalWindow)) {
+                driver.switchTo().window(windowHandle);
+                break;
+            }
+        }
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.urlToBe("https://x.com/mevlanacayde"));
+        Assert.assertEquals(driver.getCurrentUrl(),
+                "https://x.com/mevlanacayde",
+                "Redirection to Facebook page failed.");
+        driver.close();
+        driver.switchTo().window(originalWindow);
+
+        new Actions(driver).moveToElement(elements.footerInstagramIcon).click().perform();
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(driver -> driver.getWindowHandles().size() > 1);
+        for (String windowHandle : driver.getWindowHandles()) {
+            if (!windowHandle.equals(originalWindow)) {
+                driver.switchTo().window(windowHandle);
+                break;
+            }
+        }
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.urlToBe("https://www.instagram.com/mevlanacayde/#"));
+        Assert.assertEquals(driver.getCurrentUrl(),
+                "https://www.instagram.com/mevlanacayde/#",
+                "Redirection to Facebook page failed.");
+        driver.close();
+        driver.switchTo().window(originalWindow);
+
+    }
+
+    @Test (priority = 3)
+    public void PaymentMethods(){
+        Elements elements=new Elements(driver);
+        driver.get("https://mevlanacay.de/tr/");
+
+        waitForVisible(elements.footerPaymentMetIcon);
 
     }
 }
