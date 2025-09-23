70b91fa387331b9a63ca148349136374df3ba3d7
ahyat <gokdemir.aysenur@gmail.com>
2025-09-23 22:09:22 +0300

    elements güncellendi

--- src/test/java/Pages/Elements.java
+++ src/test/java/Pages/Elements.java
@@ -1,9 +1,15 @@
 package Pages;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class Elements {
+
+    public Elements(WebDriver driver){
+        PageFactory.initElements(driver,this);
+    }
     @FindBy(xpath = "//*[@title='Close']")
     public WebElement imgClose;
 
